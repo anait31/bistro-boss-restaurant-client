15696ee9f2488e98f0e1bfbf79883ce57b493004
anait31 <anaitullah198@gmail.com>
2025-02-02 15:19:51 +0800

    Add 404 page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,11 +5,13 @@ import Root from "../pages/Root/Root";
 import Home from "../pages/Home/Home";
 import OurMenu from "../pages/OurMenu/OurMenu";
 import Order from "../pages/Order/Order";
+import ErrorPages from "../pages/ErrorPages/ErrorPages";
 
   export const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root>,
+      errorElement: <ErrorPages></ErrorPages>,
       children: [
         {
             path: '/',
