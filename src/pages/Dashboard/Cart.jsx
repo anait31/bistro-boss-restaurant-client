a901287bec78f9eb9c8120ff04d3b853bcdadf2e
anait31 <anaitullah198@gmail.com>
2025-02-08 21:30:33 +0800

    Implement delete operation

--- src/pages/Dashboard/Cart.jsx
+++ src/pages/Dashboard/Cart.jsx
@@ -1,8 +1,39 @@
+import Swal from "sweetalert2";
 import userCart from "../../hooks/userCart";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const Cart = () => {
-    const [cart] = userCart();
-    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const [cart, refetch] = userCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const axiosSecure = useAxiosSecure()
+
+    const handleDelete = id => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        // console.log(res.data);
+                        if (res.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div className="p-12">
@@ -20,18 +51,18 @@ const Cart = () => {
                             <th>Image</th>
                             <th>Name</th>
                             <th>Price</th>
-                            <th>Details</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
+                        {/* row 1  */}
                         {
-                            cart.map((item, idx) => <tr>
+                            cart.map((item, idx) => <tr key={item._id}>
                                 <th>{idx + 1}</th>
-                                <td>{item?.image}</td>
+                                <td>{item.image}</td>
                                 <td>{item.name}</td>
                                 <td>{item.price}</td>
-                                <td>Details</td>
+                                <td><button onClick={() => handleDelete(item._id)}>Delete</button></td>
                             </tr>)
                         }
                     </tbody>
