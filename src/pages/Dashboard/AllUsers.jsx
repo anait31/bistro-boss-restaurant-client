402d4230685fe9af52a1a4825e4f0eec13af7407
anait31 <anaitullah198@gmail.com>
2025-02-09 20:44:15 +0800

    Update user role

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,16 +1,74 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { MdAutoDelete } from "react-icons/md";
+import { FaUsersCog } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
 
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users')
             return res.data
         }
     })
+
+    const handleUpdateUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to make admin!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, make admin!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/users/admin/${user?._id}`)
+                    .then(res => {
+                        // console.log(res.data);
+                        if (res.data.modifiedCount) {
+                            refetch()
+                            Swal.fire({
+                                title: "Success!",
+                                text: "Your user has admin now.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
+    const handleDeleteUser = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user?._id}`)
+                    .then(res => {
+                        // console.log(res.data);
+                        if (res.data.deletedCount) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+
+    }
 
     return (
         <div>
@@ -25,16 +83,20 @@ const AllUsers = () => {
                         <tr>
                             <th>##</th>
                             <th>Name</th>
+                            <th>Role</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1  */}
                         {
-                            users.map((item, idx) => <tr key={item._id}>
+                            users.map((user, idx) => <tr key={user._id}>
                                 <th>{idx + 1}</th>
-                                <td>{item.name}</td>
-                                <td><button>Update</button></td>
+                                <td>{user.name}</td>
+                                <td>{
+                                    user.role === 'admin' ? 'Admin' : <button onClick={() => handleUpdateUser(user)} className="text-2xl"><FaUsersCog></FaUsersCog></button>
+                                    }</td>
+                                <td><button onClick={() => handleDeleteUser(user)} className="text-2xl"><MdAutoDelete></MdAutoDelete></button></td>
                             </tr>)
                         }
                     </tbody>
